6cfc551737bb8b9bc80bf186ec2427a0d161b2aa
Behdad001 <132004339+Behdad001@users.noreply.github.com>
2023-04-29 12:37:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,3 +74,11 @@ streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
 
 streamlit.dataframe(my_data_row)
+
+
+
+my_data_row = my_cur.fetchall()
+streamlit.text("The fruit load list contains:")
+streamlit.dataframe(my_data_row)
+
+
